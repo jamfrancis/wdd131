d446e44537185e1b3aa5b9c6e9387a97f7fcbd51
jamfrancis <145678104+jamfrancis@users.noreply.github.com>
2024-11-13 20:48:25 -0700

    Completed Event Handling

--- week02/bom/scripts/bom.js
+++ week02/bom/scripts/bom.js
@@ -5,7 +5,20 @@ const list = document.querySelector('list');
 const li = document.createElement('li');
 const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') { 
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
+        input.focus();     
+    }
+  });
+
+  deleteButton.addEventListener('click', function () {
+    list.removeChild(li);
+    input.focus();
+    input.value = '';
+    input.focus();
+  });
